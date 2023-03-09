6e983387dde1ed599e0586bbe4d4754342816968
cpichaud <camillepichaud44@gmail.com>
2023-03-09 16:19:53 +0100

    feat: add link

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\TrickType;
 use App\Repository\TrickRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,6 +19,23 @@ class TricksController extends AbstractController
 
         return $this->render('tricks\detail_tricks.html.twig', [
             'detailTrick' => $detailTrick,
+        ]);
+    }
+
+    #[Route('/edit/{tricks}', name: '_profiler_edit_trick')]
+    public function editTrick(TrickRepository $trick, $tricks, Request $request): Response
+    {
+        $editTrick = $trick->findOneBy(array('id' => $tricks));
+
+        $form = $this->createForm(TrickType::class, $tricks);
+        //$form->handleRequest($request);
+
+        
+       // $pictures = $trick->getPictures();
+
+        return $this->render('tricks\edit_tricks.html.twig', [
+            'editTrick' => $editTrick,
+            'form' => $form->createView(),
         ]);
     }
 }
