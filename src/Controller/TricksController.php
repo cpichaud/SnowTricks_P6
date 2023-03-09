7093b8ee9c99d281b4d57f859955c2cd4c2713b4
cpichaud <camillepichaud44@gmail.com>
2023-03-09 12:43:21 +0100

    fix: debug route

---
+++ src/Controller/TricksController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TrickRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TricksController extends AbstractController
+{
+    #[Route('/{tricks}', name: '_profiler_detail_trick', methods: ['GET'])]
+    public function showTrick(TrickRepository $trick, Request $request, $tricks): Response
+    {
+        $trickstest = $trick->findOneBy(array('name' => $tricks));
+       // $pictures = $trick->getPictures();
+
+        return $this->render('triks/detail_tricks.html.twig', [
+            'trickstest' => $trickstest,
+        ]);
+    }
+}
