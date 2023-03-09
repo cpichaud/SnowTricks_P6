2d3bbbdc621636999f60219206d243316aa17aa7
cpichaud <camillepichaud44@gmail.com>
2023-03-09 15:46:45 +0100

    feat: add info trick for detail trick

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -13,11 +13,11 @@ class TricksController extends AbstractController
     #[Route('/{tricks}', name: '_profiler_detail_trick', methods: ['GET'])]
     public function showTrick(TrickRepository $trick, Request $request, $tricks): Response
     {
-        $trickstest = $trick->findOneBy(array('title' => $tricks));
+        $detailTrick = $trick->findOneBy(array('title' => $tricks));
        // $pictures = $trick->getPictures();
 
         return $this->render('tricks\detail_tricks.html.twig', [
-            'trickstest' => $trickstest,
+            'detailTrick' => $detailTrick,
         ]);
     }
 }
